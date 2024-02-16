c390e81002cf5d9cfdabb957276600b36e626251
Soyunnlee <soxxy95@naver.com>
2024-02-17 00:20:49 +0900

    Feat:HoverCard

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -10,6 +10,48 @@ function Main(props) {
       id='0'
     >
       <Mainbg />
+      {/* Fix Hover Card */}
+      <div className='top-0 bottom-0 left-0 right-0 flex items-center justify-center '>
+        <div class='card'>
+          <div class='wrapper'>
+            <img
+              src='https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg'
+              class='cover-image'
+              alt=''
+            />
+          </div>
+          <img
+            src='https://ggayane.github.io/css-experiments/cards/dark_rider-title.png'
+            class='title'
+            alt=''
+          />
+          <img
+            src='https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp'
+            class='character'
+            alt=''
+          />
+        </div>
+
+        <div class='card'>
+          <div class='wrapper'>
+            <img
+              src='https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg'
+              class='cover-image'
+              alt=''
+            />
+          </div>
+          <img
+            src='https://ggayane.github.io/css-experiments/cards/force_mage-title.png'
+            class='title'
+            alt=''
+          />
+          <img
+            src='https://ggayane.github.io/css-experiments/cards/force_mage-character.webp'
+            class='character'
+            alt=''
+          />
+        </div>
+      </div>
     </div>
   );
 }
