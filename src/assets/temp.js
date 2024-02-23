7a2942a6312c523d92e977fb67c0ee2a95149939
Soyunnlee <soxxy95@naver.com>
2024-02-24 01:02:42 +0900

    WebSite

--- src/assets/temp.js
+++ src/assets/temp.js
@@ -100,66 +100,58 @@ export const FrontEnd = [
 export const WebSite = [
   {
     id: 0,
-    title: 'SITA',
-    content:
-      '제작기간 : 2일 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
+    title: 'Svelte & Tailwind Document',
+    content: 'SvelteKit , JavaScript',
+    contentLine2: 'Disign: 100% | Develop : 100%',
     imgSrc: SvelteTailwind,
-    url: 'http://project1.ivyro.net/',
+    url: 'https://dashboard-svelte-document.netlify.app/dashboard',
   },
   {
     id: 1,
-    title: 'BEAUTY ACADEMY',
-    content:
-      '제작기간 : 2일 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: miyoung,
-    url: 'http://kwons41632.ivyro.net/',
+    title: 'Lush',
+    content: 'Html ,JavaScript, Css',
+    contentLine2: 'Disign: 100% | Develop : 100%',
+    imgSrc: lush,
+    url: 'https://lushbucket.netlify.app/',
   },
   {
     id: 2,
-    title: 'LUSH',
-    content:
-      '제작기간 : 1.5일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: lush,
-    url: 'https://soxxy95.ivyro.net/lush.ex5/lush.html',
+    title: 'OLIVE YOUNG',
+    content: 'Html ,JavaScript, Css',
+    contentLine2: 'Disign: 100% | Develop : 100%',
+    imgSrc: olive,
+    url: 'https://classy-dodol-a8c9dc.netlify.app/',
   },
   {
     id: 3,
-    title: 'OLIVE YOUNG',
-    content:
-      '제작기간 : 1.5일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: olive,
-    url: 'https://soxxy95.ivyro.net/oliveyoung.ex4/newoliveyoung.html',
-  },
-  {
-    id: 4,
     title: 'ZRAR',
-    content:
-      '제작기간 : 1일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
+    content: 'Html ,JavaScript, Css',
+    contentLine2: 'Disign: 100% | Develop : 100%',
     imgSrc: zara,
-    url: 'https://soxxy95.ivyro.net/zara.ex9/zara.html',
+    url: 'https://roaring-cupcake-4c5faf.netlify.app/',
   },
-  {
-    id: 5,
-    title: 'CHANEL',
-    content:
-      '제작기간 : 2일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: chanel,
-    url: 'https://soxxy95.ivyro.net/canel.ex2/chanel.html',
-  },
-  {
-    id: 6,
-    title: 'VOUGE',
-    content:
-      '제작기간 : 1일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: vouge,
-    url: 'https://soxxy95.ivyro.net/vogue.ex3/vogue.html',
-  },
-  {
-    id: 7,
-    title: 'SULBING',
-    content:
-      '제작기간 : 1일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
-    imgSrc: sulbing,
-    url: 'https://soxxy95.ivyro.net/vogue.ex3/vogue.html',
-  },
+  // {
+  //   id: 5,
+  //   title: 'CHANEL',
+  //   content:
+  //     '제작기간 : 2일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
+  //   imgSrc: chanel,
+  //   url: 'https://soxxy95.ivyro.net/canel.ex2/chanel.html',
+  // },
+  // {
+  //   id: 6,
+  //   title: 'VOUGE',
+  //   content:
+  //     '제작기간 : 1일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
+  //   imgSrc: vouge,
+  //   url: 'https://soxxy95.ivyro.net/vogue.ex3/vogue.html',
+  // },
+  // {
+  //   id: 7,
+  //   title: 'SULBING',
+  //   content:
+  //     '제작기간 : 1일 , 이벤트홈페이지 \n사용기술 : html, css, javascript \nDesign:100% | publising:100%',
+  //   imgSrc: sulbing,
+  //   url: 'https://soxxy95.ivyro.net/vogue.ex3/vogue.html',
+  // },
 ];
--- src/ui/SlideItem.jsx
+++ src/ui/SlideItem.jsx
@@ -1,5 +1,5 @@
 export function SlideItem(props) {
-  const { title, content, url, imgSrc } = props.site;
+  const { title, content, contentLine2, url, imgSrc } = props.site;
 
   return (
     <div class='relative p-5'>
@@ -18,27 +18,48 @@ export function SlideItem(props) {
       </div>
       {/* Text Content */}
       {/* bg-[#141517 */}
-      <div class='absolute bottom-0 left-0 bg-[#141516] opacity-60 w-full text-white flex flex-row items-center justify-between px-5 py-4'>
-        <div class='flex flex-row  space-x-5'>
-          <span class=''>{title}</span>
+      <div class='absolute bottom-0 left-0 bg-[#141516] opacity-95 w-full text-white flex flex-row items-center justify-between px-5 py-2'>
+        <div class='flex flex-row  space-x-5 items-center'>
+          <span class='font-semibold'>{title}</span>
 
-          <div class='w-[2px] h-full bg-gradient-to-b from-transparent via-white to-transparent'>
+          <div class='w-[1.8px] h-full bg-gradient-to-b from-transparent via-white to-transparent'>
             &nbsp;
           </div>
 
-          <span class=''>{content}</span>
+          <div class='flex flex-col'>
+            <span class='text-sm'>{content}</span>
+            <span class='text-sm'>{contentLine2}</span>
+          </div>
         </div>
 
         <div>
           <button
             type='button'
-            class='ItmBtn bg-black'
+            class='ItmBtn bg-[#1E202E] '
             onClick={() => {
               console.log(url);
               window.open(url, '_blank');
             }}
           >
-            사이트 바로가기
+            <div class='flex flex-row items-center justify-center space-x-1 text-[#A7A7CC]'>
+              <div>View</div>
+              <div>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={2}
+                  stroke='currentColor'
+                  className='w-3'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5'
+                  />
+                </svg>
+              </div>
+            </div>
           </button>
         </div>
       </div>
