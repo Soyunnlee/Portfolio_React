84eba387a02409dca9920be83a35ec421bfe732f
Soyunnlee <soxxy95@naver.com>
2024-02-23 23:54:25 +0900

    WebSite 수정중

--- src/component/WebSite.jsx
+++ src/component/WebSite.jsx
@@ -10,7 +10,7 @@ import { SlideContainer } from '../ui/SlideContainer';
 function webSite(props) {
   return (
     <div id='1' className='h-[100vh] flex items-center justify-center bg-black'>
-      <div className='w-[60%] mx-auto border'>
+      <div className='w-[60%] mx-auto  bg-[#141517] rounded-xl'>
         <SlideContainer WebSite={WebSite} />
       </div>
     </div>
--- src/ui/SlideItem.jsx
+++ src/ui/SlideItem.jsx
@@ -2,37 +2,45 @@ export function SlideItem(props) {
   const { title, content, url, imgSrc } = props.site;
 
   return (
-    <div class="ItmWrap">
-      <div class="">
-
-        {/* Img */}
+    <div class='relative p-5'>
+      {/* Img */}
+      <div class='w-full h-full'>
         <a
           onClick={() => {
             console.log(url);
-            window.open(url, "_blank");
+            window.open(url, '_blank');
           }}
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-        > 
-        <img src={imgSrc} />
+          data-mdb-ripple='true'
+          data-mdb-ripple-color='light'
+        >
+          <img src={imgSrc} />
         </a>
+      </div>
+      {/* Text Content */}
+      {/* bg-[#141517 */}
+      <div class='absolute bottom-0 left-0 bg-[#141516] opacity-60 w-full text-white flex flex-row items-center justify-between px-5 py-4'>
+        <div class='flex flex-row  space-x-5'>
+          <span class=''>{title}</span>
 
-          {/* Text Content */}
-        <div class="ItmTextBox">
-          <h5 class="ItmTitle">{title}</h5>
-          <p class="ItmContent">{content}</p>
+          <div class='w-[2px] h-full bg-gradient-to-b from-transparent via-white to-transparent'>
+            &nbsp;
+          </div>
+
+          <span class=''>{content}</span>
+        </div>
+
+        <div>
           <button
-            type="button"
-            class="ItmBtn bg-black"
+            type='button'
+            class='ItmBtn bg-black'
             onClick={() => {
               console.log(url);
-              window.open(url, "_blank");
+              window.open(url, '_blank');
             }}
           >
             사이트 바로가기
           </button>
         </div>
-
       </div>
     </div>
   );
